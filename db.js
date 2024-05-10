77c0ae6d9e25b752a5bd830791b5908f79a451f3
simanzler <simanzler@gmail.com>
2024-05-10 09:50:27 -0400

    Made some updates to use the body for express

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
     host: 'db',
-    port: 4000,
+    port: 5432,
     user: 'sigh',
     password: 'sigh123!',
     database: 'db'
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
     try {
         const data = await pool.query('SELECT * FROM profiles');
-        res.sendStatus(200).send(data.rows);
+        res.send(data.rows);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
@@ -15,10 +17,11 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
+    console.log("body: " + req.body);
     const { full_name, username } = req.body;
     try {
         await pool.query('INSERT INTO profiles (full_name, username) VALUES ($1, $2)', [full_name, username]);
-        res.sendStatus(200).send({ message: "Successfully added profile" });
+        res.send({ message: "Successfully added profile" });
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
@@ -28,7 +31,7 @@ app.post('/', async (req, res) => {
 app.get('/setup', async (req, res) => {
     try {
         await pool.query('CREATE TABLE profiles (id SERIAL PRIMARY KEY, full_name VARCHAR(100), username VARCHAR(100))')
-        res.sendStatus(200).send({message: "Successfully added table"});
+        res.send({message: "Successfully added table"});
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
