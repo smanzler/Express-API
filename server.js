206fc8a4028b66952684e70c7588e67b5a8e02ce
simanzler <simanzler@gmail.com>
2024-05-10 20:47:10 -0400

    Changed endpoint and added delete

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/profile', async (req, res) => {
     try {
         const data = await pool.query('SELECT * FROM profiles');
         res.send(data.rows);
@@ -16,12 +16,34 @@ app.get('/', async (req, res) => {
     }
 })
 
-app.post('/', async (req, res) => {
+app.get('/profile/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await pool.query('SELECT * FROM profiles WHERE id = $1', [id]);
+        res.send(data.rows);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.post('/profile', async (req, res) => {
     console.log("body: " + req.body);
     const { full_name, username } = req.body;
     try {
         await pool.query('INSERT INTO profiles (full_name, username) VALUES ($1, $2)', [full_name, username]);
-        res.send({ message: "Successfully added profile" });
+        res.send({ message: `Successfully added profile for ${full_name}` });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.delete('/profile/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await pool.query('DELETE FROM profiles WHERE id = $1', [id]);
+        res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
